d02d8cb692fd85a6304dab36224543d3999dfba0
RLS1000 <ryan@lopezsolis.de>
2025-04-25 20:58:32 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AngebotPage from "./pages/AngebotPage"; // ← Pfad anpassen, falls nötig!
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/angebot/:token" element={<AngebotPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
